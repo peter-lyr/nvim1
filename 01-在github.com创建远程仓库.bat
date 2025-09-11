4f0fe93d8f0594fa5af72d5bd25df05c23724087
peter-lyr <llydrp.ldp@gmail.com>
2025-09-11 22:01:33 +0800

    s5

--- 01-在github.com创建远程仓库.bat
+++ 01-在github.com创建远程仓库.bat
@@ -1,55 +1,16 @@
 @echo off
-chcp 936
-REM import os
-REM def run_print_cmd(cmd):
-REM     print(f"+++++ {cmd}")
-REM     os.system(cmd)
+chcp 65001
 
-REM repo = ''
-REM public = 'public'
-REM root_tail = ''
-REM path = ''
-
-REM # # 没有.git
-REM # cmds = f'''
-REM # git init
-REM # git add .
-REM # git commit -m "s1"
-REM # gh repo create {repo} --{public} --description "{root_tail}/{path}" --source=. --remote=origin
-REM # git branch -M main
-REM # git remote add origin git@github.com:peter-lyr/test-2.git
-REM # git push -u origin main
-REM # '''
-
-git init
+REM git init
 git add .
-git commit -m "s1"
-gh repo create nvim1 --public --description "nvim1" --source=. --remote=origin
-git branch -M main
+REM git commit -m "s1"
+REM gh repo create nvim1 --public --description "nvim1" --source=. --remote=origin
+REM git branch -M main
 REM git remote add origin git@github.com:peter-lyr/nvim1.git
 REM git push -u origin main
+git commit -m "s5"
 
-REM # root: C:\Users\depei_liu\AppData\Local\nvim\leader\leader_g
-REM # path: lwekj
-REM # public: private
-REM # name: peter-lyr
-REM # root_tail: leader_g
-REM # repo: lwekj
-
-REM # for cmd in cmds.strip().split('\n'):
-REM #     print(cmd.strip())
-
-REM # 有.git
-REM # git remote add origin git@github.com:peter-lyr/test-2.git
-REM # git branch -M main
-REM # git push -u origin main
-
-REM # run_print_cmd("git init")
-REM # run_print_cmd("git add .")
-REM # run_print_cmd('git commit -m "s1"')
-REM # run_print_cmd(
-REM #     f'gh repo create {repo} --{public} --description "{root_tail}/{path}" --source=. --remote=origin'
-REM # )
-REM # run_print_cmd('git branch -M main')
-REM # run_print_cmd(f'git remote add origin git@github.com:{name}/{repo}')
-REM # run_print_cmd("git push -u origin main")
+REM 有.git
+REM git remote add origin git@github.com:peter-lyr/test-2.git
+REM git branch -M main
+REM git push -u origin main
