93453bd1bdfb6073b054ed3c062747b817a9c153
peter-lyr <llydrp.ldp@gmail.com>
2025-09-11 22:04:31 +0800

    init.lua

---
+++ cmds1/01-在github.com创建远程仓库.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 65001
+
+REM git init
+git add .
+REM git commit -m "s1"
+REM gh repo create nvim1 --public --description "nvim1" --source=. --remote=origin
+REM git branch -M main
+REM git remote add origin git@github.com:peter-lyr/nvim1.git
+REM git push -u origin main
+git commit -m "init.lua"
+git push
+
+REM 有.git
+REM git remote add origin git@github.com:peter-lyr/test-2.git
+REM git branch -M main
+REM git push -u origin main
