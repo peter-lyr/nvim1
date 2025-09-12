9e99b8ad31e34322b981be4b1510be57daa81820
peter-lyr <llydrp.ldp@gmail.com>
2025-09-12 14:24:44 +0800

    增加leader_*，并注释掉没实际增加的

--- cmds1/02-在nvim1-lua-plugins1.lua文件下生成leader插件.py
+++ cmds1/02-在nvim1-lua-plugins1.lua文件下生成leader插件.py
@@ -3,13 +3,13 @@ for i in range(97, 97+26):
     if i in ['j', 'k']:
         continue
     a = f'''
-  -- leader_{i}
-  {{
-    name = 'leader_{i}',
-    dir = Nvim1Leader .. 'leader_{i}',
-    keys = {{
-      {{ '<leader>{i}', desc = 'leader_{i}', }},
-    }},
-  }},
+  -- -- leader_{i}
+  -- {{
+  --   name = 'leader_{i}',
+  --   dir = Nvim1Leader .. 'leader_{i}',
+  --   keys = {{
+  --     {{ '<leader>{i}', desc = 'leader_{i}', }},
+  --   }},
+  -- }},
     '''
     print('  ' + a.strip() + "\n")
