539e1638f06dc3c27e54809b5a3ca66aae5d8468
peter-lyr <llydrp.ldp@gmail.com>
2025-09-14 21:02:03 +0800

    <leader>oi打开lua.init文件

---
+++ cmds1/05-打开nvim-qt.bat
@@ -0,0 +1,1 @@
+nvim-qt -- -u C:\Users\ldp\Dp1\lazy\nvim1\init-qt.vim
---
+++ cmds1/06-提交git修改到远程.py
@@ -0,0 +1,10 @@
+import os
+import tempfile
+temp_dir = tempfile.gettempdir()
+content = "<leader>oi打开lua.init文件"
+git_commit_txt = os.path.join(temp_dir, 'git_commit.txt')
+with open(git_commit_txt, 'wb') as f:
+    f.write(content.encode('utf-8'))
+os.system("git add .")
+os.system(f"git commit -F {git_commit_txt}")
+os.system("git push")
