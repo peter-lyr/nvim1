eddc6564bcb32069b244e184a30ec2075dcb5a73
peter-lyr <llydrp.ldp@gmail.com>
2025-09-15 22:04:00 +0800

    pip_install('pyperclip')

--- cmds1/08-pip-install.py
+++ cmds1/08-pip-install.py
@@ -1,4 +1,5 @@
 import os
 def pip_install(plugin):
     os.system(f'pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host mirrors.aliyun.com {plugin}')
-# pip_install('neovim')
+# pip_install('neovim') # nvim-qt的依赖
+# pip_install('pyperclip') # 复制文本到系统剪贴板
