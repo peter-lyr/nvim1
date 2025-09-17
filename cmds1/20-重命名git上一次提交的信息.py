4edcecc5db050ba24027cd41cd204282ab8bde20
peter-lyr <llydrp.ldp@gmail.com>
2025-09-17 14:32:43 +0800

    20-重命名git上一次提交的信息.py

---
+++ cmds1/20-重命名git上一次提交的信息.py
@@ -0,0 +1,7 @@
+import os
+
+if __name__ == "__main__":
+    print(os.getcwd())
+    os.chdir(os.path.expanduser(r'~\Dp1\lazy\nvim1'))
+    # os.system('chcp 65001>nul&git commit --amend -m "19-当python重新安装后Mason需要删除重新安装.py"')
+    # os.system('git push --force')
