85e53d3d75273aefc482274c3d3479b8c897a886
peter-lyr <llydrp.ldp@gmail.com>
2025-09-17 13:07:37 +0800

    18-git递归pull子仓库.py

---
+++ cmds1/18-git递归pull子仓库.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == "__main__":
+    # print(os.getcwd())
+    # os.chdir(os.path.expanduser(r'~\Dp1\lazy\nvim1'))
+    os.system("chcp 65001>nul&git submodule update --init --recursive")
