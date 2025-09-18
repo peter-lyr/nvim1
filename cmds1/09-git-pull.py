767f41575b4c2e58b7983efa02f8803e6b0a964d
peter-lyr <llydrp.ldp@gmail.com>
2025-09-18 13:43:06 +0800

    .

--- cmds1/09-git-pull.py
+++ cmds1/09-git-pull.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == "__main__":
     print(os.getcwd())
-    os.chdir(os.path.expanduser(r'~\Dp1\lazy\nvim1'))
+    os.chdir(os.path.expanduser(r"~\Dp1\lazy\nvim1"))
     os.system("chcp 65001>nul&git pull")
