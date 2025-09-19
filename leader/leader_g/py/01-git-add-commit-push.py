10e844078ba2348f9301bfd0c723de2c0e7b66ea
peter-lyr <llydrp.ldp@gmail.com>
2025-09-19 13:50:50 +0800

    print("=========commit info:=========")

--- leader/leader_g/py/01-git-add-commit-push.py
+++ leader/leader_g/py/01-git-add-commit-push.py
@@ -10,18 +10,21 @@ if __name__ == "__main__":
         lines = f.readlines()
     push_allow = False
     git_commit_txt = git_commit_txt + ".txt"
+    print("=========commit info:=========")
     with open(git_commit_txt, "wb") as f:
         for line in lines:
             if line[:2] == b"# ":
                 continue
             if not line.strip():
                 continue
-            f.write(line.strip()+b'\n')
+            f.write(line.strip() + b"\n")
+            print(line.strip().decode("utf-8"))
             push_allow = True
+    print("==============================")
+    print(os.getcwd())
     if not push_allow:
         os.system(f"echo commit file is empty: {git_commit_txt}")
         os._exit(2)
     os.system("chcp 65001>nul & git add -A")
-    print(os.getcwd())
     os.system(f"git commit -F {git_commit_txt}")
     os.system("git push")
