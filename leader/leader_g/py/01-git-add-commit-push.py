c6847892b01e87f8adfe062ef44359854f3ef472
peter-lyr <llydrp.ldp@gmail.com>
2025-09-22 16:03:57 +0800

    打印当前commit

--- leader/leader_g/py/01-git-add-commit-push.py
+++ leader/leader_g/py/01-git-add-commit-push.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         os._exit(2)
     os.system("chcp 65001>nul & git add -A")
     os.system(f"git commit -F {git_commit_txt}")
-    print("=========commit info:=========")
-    os.system(f"git log -n 1")  # 打印当前commit
-    print("==============================")
+    # print("=========commit info:=========")
+    # os.system(f"git log -n 1")  # 打印当前commit
+    # print("==============================")
     os.system("git push")
