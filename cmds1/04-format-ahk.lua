90b04239508667a03f4818f83070c8770a9b63c9
peter-lyr <llydrp.ldp@gmail.com>
2025-10-10 15:15:17 +0800

    .

--- cmds1/04-format-ahk.lua
+++ cmds1/04-format-ahk.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_user_command("FormatAhk", function(params)
-	for _ = 1, 9 do
-		vim.cmd([[
+  for _ = 1, 9 do
+    vim.cmd([[
       try
         silent %s/^ \+$//g
       catch
@@ -26,5 +26,5 @@ vim.api.nvim_create_user_command("FormatAhk", function(params)
       catch
       endtry
     ]])
-	end
+  end
 end, { nargs = "*" })
